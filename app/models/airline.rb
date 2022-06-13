d6aa6490610ffc8564e5cbebb90a5d81d469414f
Luis Castillo <mr.luiscastillo@gmail.com>
2022-06-12 23:59:56 -0400

    add airline

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,2 +1,14 @@
 class Airline < ApplicationRecord
+    has_many :reviews
+
+    before_create :slugify
+
+    ## change the name of the parameter ex: we pass (Jetblue Airline), the slugify will pass as (jetblue-airline)
+    def slugify
+        self.slug = name.parameterize
+    end
+
+    def avg_score
+        reviews.average(:score).round(2).to_f
+    end
 end

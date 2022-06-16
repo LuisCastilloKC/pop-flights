fcb48777508499012058bb0df114f422f61a0e73
Luis Castillo <mr.luiscastillo@gmail.com>
2022-06-15 23:00:25 -0400

    add create and private method

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -10,7 +10,21 @@ module Api
             def show
                 airline = Airline.find_by(slug: params[:slug])
                 render json: AirlineSerializer.new(airline).serialized_json
+            end
 
+            def create
+                 airline = Airline.new(airline_params)
+                 if airline.save
+                    render json: AirlineSerializer.nwe(airline).serialized_json
+                 else
+                    render json: {error: airline.serros.messages}, status: 422
+                 end
+            end
+
+            private
+
+            def airline_params
+                params.require(:airline).permit(:name, :image_url)
             end
 
         end

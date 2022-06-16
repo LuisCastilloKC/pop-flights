b77e862e147a490197af657d260bdfc52670a149
Luis Castillo <mr.luiscastillo@gmail.com>
2022-06-15 22:35:47 -0400

    setup index for all Airlines

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -2,6 +2,12 @@ module Api
     module V1
         class AirlinesController < ApplicationController
 
+            def index
+                airlines = Airline.all
+
+                render json: AirlineSerializer.new(airlines).serialized_json
+            end
+
         end
     end
 end

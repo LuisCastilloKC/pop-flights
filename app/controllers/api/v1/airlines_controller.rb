857082f458d5aa4832bdab3204b2a8b9e99973fd
Luis Castillo <mr.luiscastillo@gmail.com>
2022-06-15 22:31:50 -0400

    create controller api

---
+++ app/controllers/api/v1/airlines_controller.rb
@@ -0,0 +1,7 @@
+module Api
+    module V1
+        class AirlinesController < ApplicationController
+
+        end
+    end
+end
---
+++ app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,7 @@
+module Api
+    module V1
+        class ReviewsController < ApplicationController
+
+        end
+    end
+end

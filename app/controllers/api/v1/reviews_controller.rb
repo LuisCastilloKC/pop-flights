20ef034c4dc2c597d26d167d4b2157ada64049d0
Luis Castillo <mr.luiscastillo@gmail.com>
2022-06-17 23:46:59 -0400

    add review params

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,7 +1,11 @@
 module Api
     module V1
         class ReviewsController < ApplicationController
+            private
 
+            def review_params
+                params.require(:review).permit(:title, :descripion, :score, :airline_id)
+            end
         end
     end
 end

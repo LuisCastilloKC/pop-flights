9569efbb94c35c27da286479ac2d4d4a37f983b0
Luis Castillo <mr.luiscastillo@gmail.com>
2022-06-17 23:58:43 -0400

    add destroy method for review

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,14 +1,24 @@
 module Api
     module V1
         class ReviewsController < ApplicationController
-            
+
             def create
                 review = Review.new(review_params)
 
                 if review.save
                     render json: ReviewSerializer.new(review).serialized_json
                 else
-                    render json: { errors: review.errors.message }, status: 422
+                    render json: { error: review.errors.messages }, status: 422
+                end
+            end
+
+            def destroy
+                review = Review.find_by(params[:id])
+
+                if review.destroy
+                    head :no_content
+                else
+                    render json: { error: review.errors.messages }, status: 422
                 end
             end
 
